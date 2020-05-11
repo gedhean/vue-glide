6b797b4a5c8db04e3ae97e44c5d4a2e2eddead29
Gêdhean Alves <gedhean.alves@gocase.com.br>
2020-05-11 11:55:51 -0300

    update release script

--- build/release.sh
+++ build/release.sh
@@ -1,14 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 CURRENT=$(node -p "require('./package.json').version")
 echo "Current $CURRENT"
-echo "Enter release version: "
-read VERSION
+
+VERSION=$(npm version patch)
 
 read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
 echo
+
 if [[ $REPLY =~ ^[Yy]$ ]]
+  echo "Enter release version: "
+  read VERSION
+
+  read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
+  echo
 then
   npm run build:lib
   npm version $VERSION -m "Release $VERSION"
