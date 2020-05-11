eab8d36efc3d00ec332146c9e1cf4c56cea7bdac
Gêdhean Alves <gedhean.alves@gocase.com.br>
2020-05-11 12:01:49 -0300

    input version

--- build/release.sh
+++ build/release.sh
@@ -3,18 +3,12 @@
 set -e
 CURRENT=$(node -p "require('./package.json').version")
 echo "Current $CURRENT"
-
-VERSION=$(npm version patch)
+echo "Enter release version: "
+read VERSION
 
 read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
 echo
-
 if [[ $REPLY =~ ^[Yy]$ ]]
-  echo "Enter release version: "
-  read VERSION
-
-  read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
-  echo
 then
   npm run build:lib
   npm version $VERSION -m "Release $VERSION"
